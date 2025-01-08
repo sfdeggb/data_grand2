3c17831b0ccaf00bf4f3f0158f794b6fc807951b
sfdeggb <zjmac1635@163.com>
2025-01-08 17:17:20 +0800

    fix

--- market_recomand/common/config.py
+++ market_recomand/common/config.py
@@ -1,7 +1,7 @@
 from secrets import token_hex
 from collections import defaultdict
 from logging.config import dictConfig
-
+import os
 class BaseConfig:
    SECRET_KEY = token_hex()
 
@@ -14,3 +14,36 @@ class ProductionConfig(BaseConfig):
 class TestingConfig(BaseConfig):
    TESTING = True
 
+# flask app config
+config = defaultdict(lambda: BaseConfig)
+config['dev'] = DevelopmentConfig
+config['prod'] = ProductionConfig
+config['test'] = TestingConfig
+
+# logs 目录不存在则创建
+if not os.path.exists('./logs'):
+    os.makedirs('./logs')
+
+# 日志
+dictConfig({
+    'version': 1,
+    'formatters': {
+        'default': {
+            'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+            'datefmt': '%Y-%m-%d %H:%M:%S',
+        }
+    },
+    'handlers': {
+        'file': {
+            'class': 'logging.FileHandler',
+            'level': 'INFO',
+            'filename': './logs/app.log',
+            'formatter': 'default',
+            'encoding': 'utf-8'
+        },
+    },
+    'root': {
+        'level': 'INFO',
+        'handlers': ['file']
+    }
+})
